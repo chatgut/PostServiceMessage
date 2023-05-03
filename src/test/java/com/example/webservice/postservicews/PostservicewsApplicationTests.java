ff66c3366c808c273270082ce1a758739df1a95e
Anton <lindskog96@live.se>
2023-05-03 10:08:26 +0200

    initial commit

---
+++ src/test/java/com/example/webservice/postservicews/PostservicewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice.postservicews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostservicewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
